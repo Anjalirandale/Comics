01135c29b52bc202351fd63ce8de0e1064f08486
Anjalirandale <141334404+Anjalirandale@users.noreply.github.com>
2025-04-24 19:31:56 +0530

    Delete flask-comic-website directory

--- flask-comic-website/assets/script/res.js
+++
@@ -1,5 +0,0 @@
-if(window.innerHeight>window.innerWidth) {
-	document.getElementsByTagName('style')[0].innerHTML=document.getElementsByTagName('style')[0].innerHTML+".sec{width:87%}";
-} else {
-	document.getElementsByTagName('style')[0].innerHTML=document.getElementsByTagName('style')[0].innerHTML+".sec{width:45%}";
-}
